d9edf682a8fd2a212447c29e8d845159f7f5087f
YegorAlexandrov <farlyteem@gmail.com>
2021-08-29 19:19:52 +0600

    Добавление паттернов: Facade, State, Memento

---
+++ src/com/company/statePattern/Main.java
@@ -0,0 +1,46 @@
+package com.company.statePattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Context context = new Context(new LowerCaseStat(), "Yegor");
+        context.doAction();
+        context.setState(new UpperCaseStat());
+        context.doAction();
+    }
+}
+
+interface State {
+    void doAction(Context context);
+}
+
+class LowerCaseStat implements State{
+    @Override
+    public void doAction(Context context) {
+        System.out.println(context.name.toLowerCase());
+    }
+}
+
+class UpperCaseStat implements State{
+    @Override
+    public void doAction(Context context) {
+        System.out.println(context.name.toUpperCase());
+    }
+}
+
+class Context{
+    State state;
+    String name;
+
+    public Context(State state, String name) {
+        this.state = state;
+        this.name = name;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    void doAction(){
+        state.doAction(this);
+    }
+}
